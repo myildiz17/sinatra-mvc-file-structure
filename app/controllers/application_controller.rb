c768ca3fc8aa7f5cb6c07de7dd7821e6730af767
myildiz17 <muhammetyildiz@gmail.com>
2020-10-06 08:24:16 -0500

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,26 @@
+
+class Dog
+
+  attr_accessor :name , :breed, :age
+
+  @@all=[]
+  def initialized(name, breed, age)
+      @name = name 
+      @breed = breed 
+      @age = age 
+
+      @@all << self
+  end
+
+  def self.all
+      @@all
+  end
+
+end
+
 class ApplicationController < Sinatra::Base
 
+  
   configure do
   	set :views, "app/views"
   	set :public_dir, "public"
